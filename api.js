26d10d28b69cfaa26edec0aa71cfa018049b3467
Jason Vazquez-Li <vazquezlijason@gmail.com>
2021-02-06 14:05:00 -0500

    fixes and added api endpoint

---
+++ api.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+const index = require("./index");
+
+// Used by Heroku to scape Skyscanner as defined by chron job interval
+app.get("/scrapeSkyscanner", async (req, res, next) => {
+  try {
+    res.setHeader("Content-Type", "application/json"); // Set to JSON else res will be text/html by default, which makes viewing response messy
+    const scrappedListingDataObj = await index.main();
+    res.send(scrappedListingDataObj);
+  } catch (error) {
+    console.log("GET error");
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
